3043496ed2a847c3c93d9c5aa54c2936e9e0a4e7
Jean-Dihno Trappenberg <Trapmac@Jean-Dihnos-MacBook-Pro.local>
2017-06-23 18:48:32 +0200

    .gitignore is now working

--- dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const app = new app_1.ExpressApp().start();
-const port = process.env.port || 3000;
-app.listen(port, () => {
-    console.log("Example app listening on port" + port + "!");
-});
-//# sourceMappingURL=server.js.map
